a7854f9390e6322153ac06a8cadfcfedb4e91eb7
J4k0b-p <j.porowski@o2.pl>
2024-03-07 19:08:11 +0000

    Add route for handle product ID request

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,13 @@ def render_main_template():
 @app.route('/extraction')
 def render_extraction_template():
     return render_template('extraction.html')
+
+@app.route('/submit', methods=['POST'])
+def handle_submit():
+    product_id = request.form['extractionInput']
+    print(product_id)
+    return redirect("https://www.ceneo.pl/149239580")
+   #TO DO - Change this redirect to try catch
 
 if __name__ == '__main__':
     app.run(debug=True)
