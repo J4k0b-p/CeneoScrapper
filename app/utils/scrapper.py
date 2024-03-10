9cf5dc112e6d0ce9b48b969659ed5add5ef86342
J4k0b-p <j.porowski@o2.pl>
2024-03-10 14:23:33 +0000

    Add output json

--- app/utils/scrapper.py
+++ app/utils/scrapper.py
@@ -2,6 +2,8 @@ import requests
 import math
 from flask import redirect
 from bs4 import BeautifulSoup
+import json
+import os
 
 
 def get_data(product_id):
@@ -13,29 +15,43 @@ def get_data(product_id):
         #To do : Show alert message
         return redirect("/extraction")
     elif response.status_code == 200:
-        get_main_product_info(response)
-        get_reviews_from_page(product_id,2)
+        process(product_id, response)
 
     return redirect(f"https://www.ceneo.pl/{product_id}")
 
-def get_main_product_info(response):
-    soup = BeautifulSoup(response.content, 'html.parser')
+def process(product_id,response):
+    main_info = get_main_product_info(response)
+    amount_of_pages = main_info["user_reviews_pages_amount"]
+    main_info["reviews"] = []
+    for i in range(1,amount_of_pages +1):
+        reviews_from_page = get_reviews_from_page(product_id, i)
+        main_info["reviews"].extend(reviews_from_page) 
 
+    append_product(main_info)
+
+def get_main_product_info(response):
+    product_structure = {}
+
+    soup = BeautifulSoup(response.content, 'html.parser')
     product_name_div = soup.find('div', class_ = 'product-top__title')
     reviews_amount = soup.find('div', class_ = 'score-extend__review').text
     reviews_score_span = soup.find('span', class_ = 'product-review__score')  
     reviews = reviews_amount.split()
-    print(product_name_div.text)
-    print(reviews[0])
-    print(reviews_score_span.get('content', None))
 
+    product_structure["product_name"] = product_name_div.text,
+    product_structure["reviews_amount"] = reviews[0],
+    product_structure["reviews_avg_score"] = reviews_score_span.get('content', None),
+    product_structure["user_reviews_pages_amount"] =  get_reviews_pages_amount(reviews[0])
+
+    return product_structure
 
 
 def get_reviews_pages_amount(amount):
-    return math.ceil(amount / 10)
+    return math.ceil(int(amount) / 10)
 
 #spaghetti code  -- REFACTOR NEEDED
 def get_reviews_from_page(product_id, page):
+    reviews = []
     url = f"https://www.ceneo.pl/{product_id}/opinie-{page}"
     response = requests.get(url)
     if response.status_code == 200:
@@ -44,7 +60,10 @@ def get_reviews_from_page(product_id, page):
         for review in user_reviews:
             review_id = review.get('data-entry-id')
             author_name = review.find('span', class_ = "user-post__author-name").text
-            recomendation = review.find('span', class_ = "user-post__author-recomendation").find('em').text
+            try:
+                recomendation = review.find('span', class_ = "user-post__author-recomendation").find('em').text
+            except:
+                recomendation = ""
             score = review.find('span', class_ = "user-post__score-count").text
             try:
                 purchase_confirmation = bool(review.find('div', class_ = "review-pz").find("em").text)
@@ -54,11 +73,11 @@ def get_reviews_from_page(product_id, page):
             dates_span = review.find('span',class_ = "user-post__published").find_all('time')
 
             if len(dates_span) >= 2:
-                rewiev_date = dates_span[0]['datetime']
+                reviev_date = dates_span[0]['datetime']
                 purchase_date = dates_span[1]['datetime']
 
             else:
-                rewiev_date = dates_span[0]['datetime']               
+                reviev_date = dates_span[0]['datetime']               
                 purchase_date = ""
 
             
@@ -90,10 +109,35 @@ def get_reviews_from_page(product_id, page):
                 # There is no cons_pros_list element
                 pass
 
-            print(f"Pros for product:{pros_list} ----- cons for product: {cons_list}")
+            review_structure = {
+                "review_id": review_id,
+                "author": author_name,
+                "recommendation": recomendation,
+                "stars": score,
+                "is_purchase_confirmed": purchase_confirmation,
+                "review_date": reviev_date,
+                "purchase_date": purchase_date,
+                "helpful_count": helpful_count,
+                "not_helpful_count": not_helpful_count,
+                "content": user_review,
+                "cons": cons_list,
+                "pros": pros_list
+            }
 
+            reviews.append(review_structure)
+        return reviews
+    
+def append_product(new_product):
+    file_path = "ceneo_scrapper/app/static/scrapped_product_list.JSON"
+    data = {'products': []}
+    if os.path.exists(file_path):
+        with open(file_path, 'r',encoding='utf-8') as file:
+            data = json.load(file)
+    else:
+        data['products'] = []
 
+    data['products'].append(new_product)
 
-            
-
-
+    with open(file_path, 'w',encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4) 
+        file.flush()
