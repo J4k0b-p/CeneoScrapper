f44f09916200239d6338db722646aaef0ace5790
J4k0b-p <j.porowski@o2.pl>
2024-03-10 19:47:59 +0000

    Add product parser util

---
+++ app/utils/product_parser.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+def load_data():
+    file_path = "ceneo_scrapper/app/static/scrapped_product_list.JSON"
+    data = {'products': []}
+    if os.path.exists(file_path):
+        with open(file_path, 'r',encoding='utf-8') as file:
+            data = json.load(file)
+    
+    return data
+
