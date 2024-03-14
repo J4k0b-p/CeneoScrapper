07710faee2e86175b925a0a0d84d2e59ff90408b
J4k0b-p <j.porowski@o2.pl>
2024-03-14 18:03:14 +0000

    Add product reviews table

---
+++ app/static/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#reviewsTable").DataTable({
+    paging: false,
+    scrollCollapse: true,
+    scrollY: "60vh",
+    lengthChange: true,
+    lengthMenu: [
+      [5, 10, 25, -1],
+      [5, 10, 25, "All"]
+    ],
+    ordering: true,
+    columns: [
+      { width: "10%" }, // Review ID
+      { width: "10%" }, // Author
+      { width: "5%" }, // Recommendation
+      { width: "5%" }, // Stars
+      { width: "10%" }, // Review Date
+      { width: "55%" } // Content
+      // Add or remove columns as needed, ensuring the total does not exceed 100%
+    ]
+  });
+});
